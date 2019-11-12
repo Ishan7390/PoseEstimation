0c9cdf96452bd8c22d50f08ed403ced559a40f08
IS <51106690+Ishan7390@users.noreply.github.com>
2019-11-12 23:02:14 +0530

    adding functionality for the right eye.

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ let noseX = 0;
 let noseY = 0;
 let eyelX = 0;
 let eyelY = 0;
+let eyerX = 0;
+let eyerY = 0;
  
 function setup() {
   createCanvas(640, 480);
@@ -20,10 +22,14 @@ function gotPoses(poses) {
     let nY = poses[0].pose.keypoints[0].position.y;
     let eX = poses[0].pose.keypoints[1].position.x;
     let eY = poses[0].pose.keypoints[1].position.y;
+    let EX = poses[0].pose.keypoints[2].position.x;
+    let EY = poses[0].pose.keypoints[2].position.y;
     noseX = lerp(noseX, nX, 0.5);
     noseY = lerp(noseY, nY, 0.5);
     eyelX = lerp(eyelX, eX, 0.5);
     eyelY = lerp(eyelY, eY, 0.5);
+    eyerX = lerp(eyerX, EX, 0.5);
+    eyerY = lerp(eyerY, EY, 0.5);
   }
 }
 
@@ -40,6 +46,8 @@ function draw() {
   ellipse(noseX, noseY, d/3);
   fill(0,0,255);
   ellipse(eyelX, eyelY, d/3);
+  fill(0,0,255);
+  ellipse(eyerX, eyerY, d/3);
 
 
 }
